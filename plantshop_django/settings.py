b98160b787a30576c5271e5e265f1811cec2df0e
jrkprogramming <jrkaibigan@gmail.com>
2022-09-30 18:50:36 -0500

    Deploying to vercel is not working. Going to try Netlify

--- plantshop_django/settings.py
+++ plantshop_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0m3h#y&imjn1kfe+j7szt1n^!r5ppcbn0y2kme0m*ihag+jwp*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+# ALLOWED_HOSTS = ['.vercel.app']
 
 # DATABASES = {}
 
