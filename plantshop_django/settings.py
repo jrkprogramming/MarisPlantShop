fc98faa134a261d2d31bc65dfde41dd45d14a47c
jrkprogramming <jrkaibigan@gmail.com>
2022-10-10 16:36:07 -0500

    test

--- plantshop_django/settings.py
+++ plantshop_django/settings.py
@@ -127,19 +127,20 @@ WSGI_APPLICATION = 'plantshop_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DATABASE_NAME'),
-#         'USER': config('DATABASE_USER'),
-#         'PASSWORD': config('DATABASE_PASS'),
-#         'HOST': 'database-1.ctob6xjo1yan.us-east-1.rds.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASS'),
+        'HOST': 'database-1.ctob6xjo1yan.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://jrkaibigan:Jofelryan12@database-1.ctob6xjo1yan.us-east-1.rds.amazonaws.com/plantshop')}
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
